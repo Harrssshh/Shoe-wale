8eda4e506cc7d42cdd0a9e36e00e71bf1716cb70
Harsh Dangi <harshdangi208@gmail.com>
2026-06-13 19:38:07 +0530

    update

--- components/ShoeImage.tsx
+++ components/ShoeImage.tsx
@@ -1,5 +1,6 @@
 import Image, { ImageProps } from "next/image";
 
 export function ShoeImage({ alt, ...props }: ImageProps) {
+  // eslint-disable-next-line jsx-a11y/alt-text
   return <Image alt={alt} {...props} />;
 }
